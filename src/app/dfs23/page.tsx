8a8671225d482bd9d95249e9e5887f64ed369837
Jerome Leclanche <jerome@leclan.ch>
2023-10-22 15:32:48 +0200

    Add DFS stub page

---
+++ src/app/dfs23/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DFSSummitPage: React.FC = () => (
+	<div>
+		<h1>Stripe Payments for AI Chatbots</h1>
+		<section>
+			<h2>Welcome, DFS attendees!</h2>
+			<p>Interested in integrating payments in your AI chatbot? Talk to us!</p>
+		</section>
+	</div>
+);
+
+export default DFSSummitPage;
