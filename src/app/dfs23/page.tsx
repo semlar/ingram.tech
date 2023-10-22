3a8a75cdb2c0ec9387de7b7766fbfc4f277b69b9
David Stewart <teslagraafs@gmail.com>
2023-10-22 17:12:58 -0400

    Add Ingram logo and placeholder text to dfs23 page

--- src/app/dfs23/page.tsx
+++ src/app/dfs23/page.tsx
@@ -1,12 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 const DFSSummitPage: React.FC = () => (
-	<div>
-		<h1>Stripe Payments for AI Chatbots</h1>
-		<section>
-			<h2>Welcome, DFS attendees!</h2>
-			<p>Interested in integrating payments in your AI chatbot? Talk to us!</p>
-		</section>
+	<div className={`bg-blue-dark min-h-screen`}>
+		<div className="max-w-3xl mx-auto px-8 space-y-8 text-right">
+			<Image src="/ingram_logo.png" alt="Ingram Logo" width={1362} height={320} />
+			<h1 className="text-6xl">
+				Stripe Payments
+				<br />
+				<b>for AI Chatbots</b>
+			</h1>
+			<section>
+				<p>Embed Payments in your LLM</p>
+				<p>Reduce Payments Friction</p>
+				<p>Enable new ways to pay</p>
+				<p>Increate Engagement</p>
+			</section>
+			<section>[QR Image Placeholder]</section>
+			<section>[Fintech Image Placeholder] [Strip Image Placeholder]</section>
+		</div>
 	</div>
 );
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			colors: {
+				"blue-dark": "#101733",
+			},
 		},
 	},
 	plugins: [],
