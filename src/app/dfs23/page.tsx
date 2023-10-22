633e2dc8975ac25b6f19e24aedde3cb3b2227a67
David Stewart <teslagraafs@gmail.com>
2023-10-22 18:09:01 -0400

    Add stripe and ftbe logos to dtfs23 page

--- src/app/dfs23/page.tsx
+++ src/app/dfs23/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import React from "react";
 
 const DFSSummitPage: React.FC = () => (
-	<div className={`bg-blue-dark min-h-screen`}>
+	<div className="bg-blue-dark min-h-screen">
 		<div className="max-w-3xl mx-auto px-8 space-y-8 text-right">
 			<Image src="/ingram_logo.png" alt="Ingram Logo" width={1362} height={320} />
 			<h1 className="text-6xl">
@@ -17,7 +17,20 @@ const DFSSummitPage: React.FC = () => (
 				<p>Increate Engagement</p>
 			</section>
 			<section>[QR Image Placeholder]</section>
-			<section>[Fintech Image Placeholder] [Strip Image Placeholder]</section>
+			<section className="flex gap-x-8 items-center justify-end">
+				<Image
+					src="/ftbe_logo.png"
+					width={180}
+					height={53}
+					alt="Fintech Belgium Logo"
+				/>
+				<Image
+					src="/stripe_logo.png"
+					width={180}
+					height={76}
+					alt="Stripe Logo"
+				/>
+			</section>
 		</div>
 	</div>
 );
