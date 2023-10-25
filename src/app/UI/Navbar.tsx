626d13c82bd9e6d65c463be56c34031b8872864d
David Stewart <teslagraafs@gmail.com>
2023-10-25 09:42:44 -0400

    Remove dark mode theme from navbar

--- src/app/UI/Navbar.tsx
+++ src/app/UI/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Navbar = () => {
 	return (
-		<nav className="bg-transparent border-gray-200 dark:bg-gray-900">
+		<nav className="bg-transparent border-gray-200">
 			<div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 				<Link className="flex items-center" href="/">
 					<Image
@@ -17,7 +17,7 @@ const Navbar = () => {
 				<button
 					data-collapse-toggle="navbar-default"
 					type="button"
-					className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+					className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
 					aria-controls="navbar-default"
 					aria-expanded="false"
 				>
@@ -39,11 +39,11 @@ const Navbar = () => {
 					</svg>
 				</button>
 				<div className="hidden w-full md:block md:w-auto" id="navbar-default">
-					<ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border  rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+					<ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border  rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0">
 						<li>
 							<a
 								href="/"
-								className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-red-500 md:p-0 dark:text-white md:dark:text-blue-500"
+								className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-red-500 md:p-0"
 								aria-current="page"
 							>
 								Home
@@ -52,7 +52,7 @@ const Navbar = () => {
 						<li>
 							<Link
 								href="/About"
-								className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+								className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0"
 							>
 								About
 							</Link>
@@ -60,7 +60,7 @@ const Navbar = () => {
 						<li>
 							<Link
 								href="/Team"
-								className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+								className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0"
 							>
 								The Team
 							</Link>
