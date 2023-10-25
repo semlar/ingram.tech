5fe555c4d2d6f8c54bbb6f991efe9f9a561a4f25
David Stewart <teslagraafs@gmail.com>
2023-10-25 09:28:04 -0400

    Remove reference to unused font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import {Victor_Mono} from "next/font/google";
 import CTA from "./Home/CTA";
 import Content from "./Home/Content";
 import Intro from "./Home/Intro";
 import Knowledge from "./Home/Knowledge";
-
-const monofont = Victor_Mono({subsets: ["latin"]});
-const monospace = monofont.className;
 
 const HomePage = () => (
 	<>
