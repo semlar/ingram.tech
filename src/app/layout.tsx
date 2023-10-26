e4db23af4e233de19df265546ea3414be3efb7ce
David Stewart <teslagraafs@gmail.com>
2023-10-26 07:56:14 -0400

    Fix import paths for layout components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
-import Footer from "./UI/Footer";
-import Navbar from "./UI/Navbar";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import "./globals.css";
 
 const inter = Inter({subsets: ["latin"]});
