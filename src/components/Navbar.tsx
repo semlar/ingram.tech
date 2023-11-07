5b4dfcb8b23f08ea0341e2951e90ee8e770a0365
enzog96 <enzogrillo96@gmail.com>
2023-11-07 17:22:12 +0100

    responsive title for mobile on home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 				<button
 					data-collapse-toggle="navbar-default"
 					type="button"
-					className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+					className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100  focus:outline-none focus:ring-2 focus:ring-gray-200"
 					aria-controls="navbar-default"
 					aria-expanded="false"
 				>
@@ -52,7 +52,7 @@ const Navbar = () => {
 						<li>
 							<Link
 								href="/product"
-								className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0"
+								className="block py-2 pl-3 pr-4 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0"
 							>
 								ToolBox
 							</Link>
@@ -60,7 +60,7 @@ const Navbar = () => {
 						<li>
 							<Link
 								href="/faraday"
-								className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0"
+								className="block py-2 pl-3 pr-4 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0"
 							>
 								Faraday
 							</Link>
@@ -68,7 +68,7 @@ const Navbar = () => {
 						<li>
 							<Link
 								href="/team"
-								className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0"
+								className="block py-2 pl-3 pr-4 text-white rounded hover:bg-blue-700 md:hover:bg-transparent md:border-0 md:hover:text-red-500 md:p-0"
 							>
 								The Team
 							</Link>
