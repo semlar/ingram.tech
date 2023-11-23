15ae45e74494c48ffa982838b15bfff07169ac6e
David Stewart <teslagraafs@gmail.com>
2023-11-23 08:24:03 -0500

    Remove AI tech section from home page

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -3,12 +3,10 @@ import Content from "./sections/Content";
 import Intro from "./sections/Intro";
 import Knowledge from "./sections/Knowledge";
 import LogoBanner from "./sections/LogoBanner";
-import Technologies from "./sections/Technologies";
 
 const HomePage = () => (
 	<>
 		<Intro />
-		<Technologies />
 		<Knowledge />
 		<LogoBanner />
 		<Content />
