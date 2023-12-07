69273c60b19f09031e053a27813345b1fd62d335
Semlar <teslagraafs@gmail.com>
2023-12-07 11:58:09 -0500

    Fix redirect for DFS flyer QR code

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: "/qr/dfs-flyer-contest",
+				destination:
+					"https://share-eu1.hsforms.com/1lnmc-TltTAqlog5PMc5V6A2b7hj7",
+				permanent: false,
+			},
+		];
+	},
 	async rewrites() {
 		return [
 			{
 				source: "/qr/card",
 				destination: "/?utm_source=business-card&utm_medium=qrcode",
 			},
-			{
-				source: "/qr/dfs-flyer-contest",
-				destination: "https://share-eu1.hsforms.com/1lnmc-TltTAqlog5PMc5V6A2b7hj7",
-			}
 		];
 	},
 };
